74828c6616ebc6bcdf2722c638d99ec0d19188e0
j-goldsmith <jared.goldsmith@gmail.com>
2018-10-15 17:21:06 -0700

    couple more k-mer exercises

--- week2.py
+++ week2.py
@@ -11,14 +11,17 @@ def skew(genome):
         skew_history.append(skew)
     return skew_history
 
+
 def minimum_skew(genome):
     skew_history = np.array(skew(genome))
     return np.where(skew_history == skew_history.min())[0].tolist()
 
+
 def HammingDistance(p, q):
     p = np.array(list(p))
     q = np.array(list(q))
-    return (p!=q).sum()
+    return (p != q).sum()
+
 
 def ApproximatePatternMatching(Text, Pattern, d):
     positions = []
@@ -28,3 +31,33 @@ def ApproximatePatternMatching(Text, Pattern, d):
         if HammingDistance(Pattern, Text[i:i+pattern_length]) <= d:
             positions.append(i)
     return positions
+
+
+def ApproximatePatternCount(Text, Pattern, d):
+    return len(ApproximatePatternMatching(Text,Pattern,d))
+
+def FrequentWordsWithMismatches(Text, k, d):
+    '''Code Vomit'''
+    words = list(set([Text[i:i+k] for i in range(len(Text) - k + 1)]))
+    jimmied_words = []
+    chars = ['A','T','C','G']
+
+    for word in words:
+        for i in range(len(word)):
+            for c in chars:
+                w_list = list(word)
+                w_list[i] = c
+                jimmied_words.append("".join(w_list))
+
+    jimmied_words = list(set(jimmied_words))
+    approximate_counts = []
+    for word in jimmied_words:
+        approximate_counts.append(ApproximatePatternCount(Text, word, d))
+
+    approximate_counts = np.array(approximate_counts)
+    max_indices = np.where(approximate_counts == approximate_counts.max())[0].tolist()
+
+    return [jimmied_words[i] for i in max_indices]
+
+FrequentWordsWithMismatches('ACGTTGCATGTCGCATGATGCATGAGAGCT', 4, 1)
+FrequentWordsWithMismatches('AAAAAAAAAA', 2, 1)
